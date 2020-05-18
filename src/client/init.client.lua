756c6e87645a38f1989b840bc98eb2aab370c805
Matthew Zhu <mattzhu@amazon.com>
2020-05-18 00:58:31 -0700

    Minimal change for debugging: restore backpack GUI, re-add to starter pack

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -3,4 +3,4 @@ print("Hello world, from client!")
 local Utils = require(game.ReplicatedStorage.Scripts.Utils)
 
 Utils.logInfo(game.Players.LocalPlayer.Name .. " joined the game")
-game:GetService("StarterGui"):SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
+--game:GetService("StarterGui"):SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
